6266831c77e75819d4092894132ab158dc94a03a
Matt Johnson <mjohn23@gmail.com>
2020-05-03 20:37:19 -0500

    Reformat JSON for Greenhouse

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ def get_greenhouse_jobs(site_name):
   """
   Returns jobs from Greenhouse
   """
-  results = {}
+  results = []
 
   base_url = 'https://boards.greenhouse.io'
   url = f'{base_url}/{site_name}/'
@@ -16,33 +16,48 @@ def get_greenhouse_jobs(site_name):
   departments = soup.findAll('section', {'class': 'level-0'})
   for department in departments:
     department_name = department.find('h3').text
-    results[department_name] = {'jobs': [], 'departments': {}}
     jobs = department.findChildren('div', {'class': 'opening'}, recursive = False)
     for job in jobs:
       job_title = job.find('a').text
+
       job_link = job.find('a')['href']
       job_link = f'{base_url}{job_link}'
-      job_dict = {'job_title': job_title, 'job_link': job_link}
-      if 'jobs' in results[department_name]:
-        results[department_name]['jobs'].append(job_dict)
-      else:
-        results[department_name]['jobs'] = [job_dict]
+
+      job_id = job_link.rsplit('/', 1)[-1]
+      
+      job_dict = {
+          'id': job_id,
+          'job_title': job_title,
+          'job_url': job_link,
+          'job_location': '', 
+          'job_link': job_link,
+          'department_1': department_name,
+          'department_2': ''
+        }
+      results.append(job_dict)
 
     sub_departments = department.findChildren('section', {'class': 'level-1'})
     for sub_department in sub_departments:
       sub_department_name = sub_department.find('h4').text
-      results[department_name]['departments'][sub_department_name] = {}
       jobs = sub_department.findChildren('div', {'class': 'opening'}, recursive = False)
       for job in jobs:
         job_title = job.find('a').text
+
         job_link = job.find('a')['href']
         job_link = f'{base_url}{job_link}'
-        job_dict = {'job_title': job_title, 'job_link': job_link}
 
-        if 'jobs' in results[department_name]['departments'][sub_department_name]:
-          results[department_name]['departments'][sub_department_name]['jobs'].append(job_dict)
-        else:
-          results[department_name]['departments'][sub_department_name]['jobs'] = [job_dict]
+        job_id = job_link.rsplit('/', 1)[-1]
+
+        job_dict = {
+            'id': job_id,
+            'job_title': job_title,
+            'job_url': job_link,
+            'job_location': '', 
+            'job_link': job_link,
+            'department_1': department_name,
+            'department_2': sub_department_name
+          }
+        results.append(job_dict)
 
   return json.dumps(results)
 
